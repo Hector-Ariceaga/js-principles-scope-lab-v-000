297dc41a58eea1a367b2ae9152804582b8d365b7
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-06 00:34:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var customerName = 'bob'
 
 function upperCaseCustomerName(customerName) {
-  return var customerName = customerName.toUpperCase()
+  var name = customerName.toUpperCase()
 }
 
 function setBestCustomer() {
